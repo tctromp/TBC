61880c909694c8282da9382637eaa97e3c365b5f
takatoshiH <t.hikijiri@gmail.com>
2019-12-06 15:29:22 +0900

    Block.rbにトランザクションの検証処理を加えた

--- block.rb
+++ block.rb
@@ -22,5 +22,42 @@ def recieve_block(env)
 end
 
 def block_is_valid?(block)
-  return block["hash"].start_with?("000")
+  return hash_is_enough_small?(block) && included_transactions_are_valid?(block)
+end
+
+def hash_is_enough_small?(block)
+    block["hash"].start_with?("000")
+end
+
+def included_transactions_are_valid?(block)
+    transactons_are_valid = true
+    block["transactions"].each do |transaction|
+        unless enough_token?(ransaction)
+            transactons_are_valid = falsebreak
+        end
+    end
+    return transactons_are_valid
+end
+
+def enough_token?(transaction) 
+    enough_flag = true
+    CSV.read("./ledger.csv", headers: true).each do |account|
+        if transaction["from"] == account.to_hash["address"] && account.to_hash["amount"] <= transaction["value"].to_i
+            enough_flag = false
+            break
+        end
+        return enough_flag
+    end 
+end
+
+def account_is_recognized?(transaction)
+    recognized_flag = false
+
+    CSV.read("./ledger.csv", headers: true).each do |account|
+        if transaction["from"] == account.to_hash["address"]
+            recognized_flag = true
+            break
+        end
+        return enough_flag
+    end 
 end
