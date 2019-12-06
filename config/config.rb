135a02b797a9f7a9c440bc0bc6d1e795a30e11c0
takatoshiH <t.hikijiri@gmail.com>
2019-12-06 15:30:58 +0900

    加筆

--- config/config.rb
+++ config/config.rb
@@ -1,4 +1,5 @@
 require "./transaction.rb"
+require "./block.rb"
 
 app do |env|
   
@@ -6,14 +7,7 @@ app do |env|
   when "/transaction"
     recieve_transaction(env)
   when "/block"
-    p "blockの処理"
-    message = "Blockを送信"
-
-    response_body = {
-      "message": message
-    }
-    [200, {'Content-Type' => 'application/json'}, [response_body.to_json]]
-
+    recieve_block(env)
   else
     message = "Not Found"
 
@@ -23,28 +17,3 @@ app do |env|
     [200, {'Content-Type' => 'application/json'}, [response_body.to_json]]
   end
 end
-
-def recieve_block(env)
-
-end
-
-# def create_block_contents
-#   sum = 0
-#   nonce = 0
-#   transactions = []
-#   block_hash = ""
-#   CSV.read("./transactions.csv", headers: true).each do |transaction|
-#     sum += transaction.to_hash["hash"].hex
-#     transactions.push(transaction)
-#   end
-
-#   while true
-#     block_hash = OpenSSL::Digest.new("sha256").update(nonce.to_s + sum.to_s).to_s
-#     if block_hash.start_with?("0000")
-
-#       break
-#     end
-#     nonce += 1
-#   end
-#   return nonce, block_hash, transactions
-# end
--- request.rb
+++ request.rb
@@ -2,26 +2,58 @@ require "httpclient"
 require "json"
 require "openssl"
 
-node_urls = ["http://127.0.0.1:4000/transaction"]
+node_urls = ["http://127.0.0.1:4000/block"]
 client = HTTPClient.new
 
-from = "0xa3e639fd35"
-to = "0xccb9ef3c64"
-value = 100
-time_stamp = Time.now.to_s
+# トランザクションを発行するときのqueryを作る作業
+# from = "0xa3e639fd35"
+# to = "0xccb9ef3c64"
+# value = 100
+# time_stamp = Time.now.to_s
 
-digest = OpenSSL::Digest.new("sha256")
-transaction_hash = digest.update(from + to + value.to_s + time_stamp).to_s.slice(1..10)
+# digest = OpenSSL::Digest.new("sha256")
+# transaction_hash = digest.update(from + to + value.to_s + time_stamp).to_s.slice(1..10)
 
+# query = {
+#     "from": from, 
+#     "to": to,
+#     "value": value,
+#     "time_stamp": time_stamp,
+#     "hash": transaction_hash
+# }.to_json
+
+# ブロックを発行するときのqueryを作る作業
+
+transactions = [{from: "0xa3e639fd35", to: "b", value: 1000000, hash: "abc", time_stamp: "2019-12-05 17:40:01 +0900"},
+                {from: "0xa3e639fd35", to: "b", value: 1, hash: "defg", time_stamp: "2019-12-05 17:40:01 +0900"}]
 query = {
-    "from": from, 
-    "to": to,
-    "value": value,
-    "time_stamp": time_stamp,
-    "hash": transaction_hash
+    "hash": "0000cdf",
+    "nonce": 100,
+    "transactions": transactions
 }.to_json
 
 node_urls.each do |url|
     res = client.post(url, query)
     puts res.body
 end
+
+# def create_block_contents
+#   sum = 0
+#   nonce = 0
+#   transactions = []
+#   block_hash = ""
+#   CSV.read("./transactions.csv", headers: true).each do |transaction|
+#     sum += transaction.to_hash["hash"].hex
+#     transactions.push(transaction)
+#   end
+
+#   while true
+#     block_hash = OpenSSL::Digest.new("sha256").update(nonce.to_s + sum.to_s).to_s
+#     if block_hash.start_with?("0000")
+
+#       break
+#     end
+#     nonce += 1
+#   end
+#   return nonce, block_hash, transactions
+# end
