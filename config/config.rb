4a31a8dab5fa92bb4baee7c9696b6ae85b8812e0
takatoshiH <t.hikijiri@gmail.com>
2019-12-06 14:10:09 +0900

    リクエストに応じて処理を変更するようにした

--- config/config.rb
+++ config/config.rb
@@ -4,35 +4,57 @@ require "httpclient"
 require "csv"
 require "openssl"
 
-app do |env| 
-  puts "Recieved transaction"
-  ip_addr = Resolv::DNS.new(:nameserver=>'ns1.google.com').getresources("o-o.myaddr.l.google.com", Resolv::DNS::Resource::IN::TXT)[0].strings[0]
-  response = false
-  message = "Transaction's format is not valid"
-
-  transaction = JSON.parse(env['rack.input'].read) rescue nil
+app do |env|
   
-  if transaction.kind_of?(Hash)
-    if transaction_is_valid?(transaction)
-      save_transaction(transaction)
-      send_transaction(transaction)
-      message = "Confirmed"
-    else
-      message = "Rejected"
-    end
-    response = true
+  case env["REQUEST_PATH"]
+  when "/transaction"
+    recieve_transaction(env)
+  when "/block"
+    p "blockの処理"
+    message = "Blockを送信"
+
+    response_body = {
+      "message": message
+    }
+    [200, {'Content-Type' => 'application/json'}, [response_body.to_json]]
+
+  else
+    message = "Not Found"
+
+    response_body = {
+      "message": message
+    }
+    [200, {'Content-Type' => 'application/json'}, [response_body.to_json]]
   end
+end
 
-  create_block()
+def recieve_transaction(env)
+  puts "Recieved transaction"
 
-  response_body = {
-    "ip_addr": ip_addr,
-    "responce": response,
-    "message": message
-  }
+    ip_addr = Resolv::DNS.new(:nameserver=>'ns1.google.com').getresources("o-o.myaddr.l.google.com", Resolv::DNS::Resource::IN::TXT)[0].strings[0]
+    response = false
+    message = "Transaction's format is not valid"
 
-  [200, {'Content-Type' => 'application/json'}, [response_body.to_json]]
+    transaction = JSON.parse(env['rack.input'].read) rescue nil
+  
+    if transaction.kind_of?(Hash)
+      if transaction_is_valid?(transaction)
+        save_transaction(transaction)
+        # send_transaction(transaction)
+        message = "Confirmed"
+      else
+        message = "Rejected"
+      end
+      response = true
+    end
 
+    response_body = {
+      "ip_addr": ip_addr,
+      "responce": response,
+      "message": message
+    }
+
+    [200, {'Content-Type' => 'application/json'}, [response_body.to_json]]
 end
 
 def transaction_is_valid?(transaction)
@@ -43,9 +65,9 @@ def transaction_is_duplicated?(transaction)
   duplicated_flag = false
 
   CSV.read("./transactions.csv", headers: true).each do |post_transaction|
-    if post_transaction.to_hash["hash"] == transaction["transaction_hash"]
+    if post_transaction.to_hash["hash"] == transaction["hash"]
       duplicated_flag = true
-      puts "Transaction is duplicated: #{transaction["transaction_hash"]}"
+      puts "Transaction is duplicated: #{transaction["hash"]}"
       break
     end
   end
@@ -68,7 +90,7 @@ def save_transaction(transaction)
   CSV.open("./transactions.csv","a") do |log|
     log.puts [transaction["from"], transaction["to"], transaction["value"], transaction["hash"], transaction["time_stamp"]]
   end
-  puts "Transaction is saved: #{transaction["transaction_hash"]}"
+  puts "Transaction is saved: #{transaction["hash"]}"
 end
 
 def send_transaction(transaction)
@@ -81,7 +103,7 @@ def send_transaction(transaction)
   end
 end
 
-def create_block
+def create_block_contents
   sum = 0
   nonce = 0
   transactions = []
