fcd5e0bab1f7054009a0b0041a236ebf462a428a
takatoshiH <t.hikijiri@gmail.com>
2019-12-09 16:44:01 +0900

    トランザクションの保存先をmysqlにした

--- transaction.rb
+++ transaction.rb
@@ -3,9 +3,20 @@ require "json"
 require "httpclient"
 require "csv"
 require "openssl"
+require "active_record"
 require "./connecter.rb"
 
-def recieve_transaction(env)
+ActiveRecord::Base.establish_connection(
+	:adapter=>"mysql2",
+	:host  =>"localhost",
+	:database =>"tbc",
+	:username=>"root",
+	:password=>"",
+)
+
+class Transaction < ActiveRecord::Base
+
+  def self.recieve_transaction(env)
   puts "Recieved transaction"
 
     ip_addr = Resolv::DNS.new(:nameserver=>'ns1.google.com').getresources("o-o.myaddr.l.google.com", Resolv::DNS::Resource::IN::TXT)[0].strings[0]
@@ -17,7 +28,7 @@ def recieve_transaction(env)
     if transaction.kind_of?(Hash)
       if transaction_is_valid?(transaction)
         save_transaction(transaction)
-        send_data("/transaction", transaction.to_json)
+        # send_data("/transaction", transaction.to_json)
         message = "Confirmed"
       else
         message = "Rejected"
@@ -32,39 +43,47 @@ def recieve_transaction(env)
     }
 
     [200, {'Content-Type' => 'application/json'}, [response_body.to_json]]
-end
+  end
 
-def transaction_is_valid?(transaction)
-  return !transaction_is_duplicated?(transaction) && enough_token?(transaction)
-end
+  def self.transaction_is_valid?(transaction)
+    return !transaction_is_duplicated?(transaction)
+    # return !transaction_is_duplicated?(transaction) && enough_token?(transaction)
+  end
 
-def transaction_is_duplicated?(transaction)
-  duplicated_flag = false
+# トランザクションが重複していないか確認
+  def self.transaction_is_duplicated?(transaction)
+    duplicated_flag = false
 
-  CSV.read("./transactions.csv", headers: true).each do |post_transaction|
-    if post_transaction.to_hash["hash"] == transaction["hash"]
-      duplicated_flag = true
-      puts "Transaction is duplicated: #{transaction["hash"]}"
-      break
+    Transaction.all.each do |post_transaction|
+      if post_transaction.transaction_hash == transaction["hash"]
+        duplicated_flag = true
+        puts "Transaction is duplicated: #{transaction["hash"]}"
+        break
+      end
     end
+    return duplicated_flag
   end
-  return duplicated_flag
-end
 
-def enough_token?(transaction)
-  enough_flag = true
-  CSV.read("./ledger.csv", headers: true).each do |account|
-    if account.to_hash["address"] == transaction["from"] && account.to_hash["amount"].to_i <= transaction["value"].to_i
-      enought_flag = false
-      break
+# 十分な通貨を持っているか確認
+  def self.enough_token?(transaction)
+    enough_flag = true
+    CSV.read("./ledger.csv", headers: true).each do |account|
+      if account.to_hash["address"] == transaction["from"] && account.to_hash["amount"].to_i <= transaction["value"].to_i
+        enought_flag = false
+        break
+      end
     end
+    return enough_flag
   end
-  return enough_flag
-end
 
-def save_transaction(transaction)
-  CSV.open("./transactions.csv","a") do |log|
-    log.puts [transaction["from"], transaction["to"], transaction["value"], transaction["hash"], transaction["time_stamp"]]
+  def self.save_transaction(transaction)
+    # CSV.open("./transactions.csv","a") do |log|
+    #   log.puts [transaction["from"], transaction["to"], transaction["value"], transaction["hash"], transaction["time_stamp"]]
+    # end
+
+    Transaction.create(from_address: transaction["from"], to_address: transaction["to"], value: transaction["value"], transaction_hash: transaction["hash"], time_stamp: transaction["time_stamp"])
+    puts "Transaction is saved: #{transaction["hash"]}"
   end
-  puts "Transaction is saved: #{transaction["hash"]}"
+  
+
 end
